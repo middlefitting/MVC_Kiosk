6b974b65697c4a944c08abf2d2780fedc71f6d1e
middlefitting <wjdtmdcjf2@gmail.com>
2022-02-07 18:15:09 +0900

    02. Member 정의 1차 완료.

--- KGKiosk/src/main/java/com/myspring/kgkiosk/member/controller/MemberController.java
+++ KGKiosk/src/main/java/com/myspring/kgkiosk/member/controller/MemberController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -17,4 +18,7 @@ public interface MemberController {
 								RedirectAttributes rAttr,
 								HttpServletRequest request, HttpServletResponse response) throws Exception;
 	public ModelAndView logout(HttpServletRequest request, HttpServletResponse response) throws Exception;
+	public ModelAndView addMember(@ModelAttribute("info") MemberVO memberVO, HttpServletRequest request, HttpServletResponse response) throws Exception;
+	public ModelAndView removeMember(@RequestParam("id") String id, HttpServletRequest request, HttpServletResponse response) throws Exception;
+	public ModelAndView modifyMember(@ModelAttribute("info") MemberVO memberVO, HttpServletRequest request, HttpServletResponse response) throws Exception;
 }
--- KGKiosk/src/main/java/com/myspring/kgkiosk/member/service/MemberServiceImpl.java
+++ KGKiosk/src/main/java/com/myspring/kgkiosk/member/service/MemberServiceImpl.java
@@ -1,6 +1,7 @@
 package com.myspring.kgkiosk.member.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,6 +23,27 @@ public class MemberServiceImpl implements MemberService {
 	@Override
 	public MemberVO login(MemberVO memberVO) throws Exception{
 		return memberDAO.loginByIdPwd(memberVO);
+	}
+	
+	@Override
+	public MemberVO adminLogin(MemberVO memberVO) throws Exception {
+		return memberDAO.adminLoginByIdPwd(memberVO);
+	}
+
+	@Override
+	public int addMember(MemberVO member) throws DataAccessException {
+		return memberDAO.insertMember(member);
+	}
+	
+	@Override
+	public int removeMember(String id) throws DataAccessException {
+		return memberDAO.deleteMember(id);
+	}
+
+
+	@Override
+	public int modifyMember(MemberVO member) throws DataAccessException {
+		return memberDAO.updateMember(member);
 	}
 
 }
